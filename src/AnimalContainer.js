042a4f72e4d3a60aaca253ec7998bedc33685f5d
Julie C <jucheng925@gmail.com>
2023-08-29 13:05:45 -0400

    finish animal container

--- src/AnimalContainer.js
+++ src/AnimalContainer.js
@@ -8,15 +8,18 @@ import React, { useState } from "react";
 
 function AnimalContainer() {
   const [animals, setAnimals] = useState([{id: 1, name: "Ron", species: "Lion", hungry: true}, {id: 2, name: "Leslie", species: "Rabbit", hungry: false}, {id: 3, name: "Ann", species: "Koala", hungry: false}, {id: 4, name: "April", species: "Hyena", hungry: true}])
+  const [displayHungry, setDisplayHungry] = useState(true)
+
+  const showHungry = animals.filter((animal) => animal.hungry)
+  const showFull = animals.filter((animal) => !animal.hungry)
 
   return (
     <div>
-      <button>Hungry Animals</button>
-      <button>Full Animals</button>
+      <button onClick={()=> setDisplayHungry(true)}>Hungry Animals</button>
+      <button onClick={()=> setDisplayHungry(false)}>Full Animals</button>
       <h3>Animals go here:</h3>
-      {animals.map((animal) => <ul key={animal.id}>{animal.name} {animal.hungry ? "is" : "is not"} hungry. </ul>)}
-       <ul>
-      </ul>
+      {/* {animals.map((animal) => <ul key={animal.id}>{animal.name} {animal.hungry ? "is" : "is not"} hungry. </ul>)} */}
+      {(displayHungry ? showHungry : showFull).map((animal) => <ul key={animal.id}>{animal.name} {animal.hungry ? "is" : "is not"} hungry. </ul>)}
     </div>
   );
 }
